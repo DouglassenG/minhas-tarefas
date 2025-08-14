4d59f6762b699917b986d62c243082364d2dd1a2
Douglas <douglasmsrodrigues@hotmail.com>
2025-08-14 12:23:07 -0300

    Aplicando uma função de pesquisa

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -6,11 +6,22 @@ import { RootReducer } from '../../store'
 
 const ListaDeTarefas = () => {
   const { itens } = useSelector((state: RootReducer) => state.tarefas)
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+
+  const filtraTerefas = () => {
+    return itens.filter(
+      (item) =>
+        item.titulo.toLocaleLowerCase().search(termo.toLocaleLowerCase()) >= 0
+    )
+  }
+
   return (
     <Container>
-      <p>2 tarefas marcadas como: &quot;categoria&quot; e &quot;termo&quot;</p>
+      <p>
+        2 tarefas marcadas como: &quot;categoria&quot; e &quot;{termo}&quot;
+      </p>
       <ul>
-        {itens.map((t) => (
+        {filtraTerefas().map((t) => (
           <li key={t.titulo}>
             <Tarefa
               id={t.id}
