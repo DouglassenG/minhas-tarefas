68570e1b7d07992fd83b7dba4e9c8c61b8506373
Douglas <douglasmsrodrigues@hotmail.com>
2025-08-20 13:38:46 -0300

    Aplicando o React Router DOM

---
+++ src/components/BotaoAdicionar/styles.ts
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+export const Circulo = styled(Link)`
+  height: 64px;
+  width: 64px;
+  background-color: #44bd32;
+  color: #fff;
+  position: fixed;
+  bottom: 40px;
+  right: 40px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center; // TRATA A POSIÇÃO DA HORIZONTAL
+  align-items: center; // TRATA A POSIÇÃO DA VERTICAL
+  font-size: 40px;
+  text-decoration: none;
+`
---
+++ src/pages/Cadastro/index.tsx
@@ -0,0 +1,12 @@
+import Formulario from '../../components/Formulario'
+import BarraLateral from '../../containers/BarraLateral'
+
+const Cadastro = () => (
+  <>
+    {/* Barra lateral */}
+    <BarraLateral />
+    <Formulario />
+  </>
+)
+
+export default Cadastro
