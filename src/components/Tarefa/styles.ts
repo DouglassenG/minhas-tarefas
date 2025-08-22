6dae4ab2450d5d68a55f286b34e60d19f2d952a1
Douglas <douglasmsrodrigues@hotmail.com>
2025-08-22 13:38:06 -0300

    Implementando o React Router

--- src/components/Tarefa/styles.ts
+++ src/components/Tarefa/styles.ts
@@ -27,11 +27,17 @@ export const Card = styled.div`
   padding: 16px;
   margin-bottom: 32px;
   border-radius: 16px;
+
+  label {
+    display: flex;
+    align-itens: center;
+    margin-bottom: 16px;
+  }
 `
 export const Titulo = styled.h3`
   font-size: 18px;
   font-weight: bold;
-  margin-bottom: 16px;
+  margin-left: 8px;
 `
 
 export const Tag = styled.span<TagProps>`
@@ -76,9 +82,7 @@ export const Botao = styled.button`
   border-radius: 8px;
   margin-right: 8px;
 `
-export const BotaoSalvar = styled(Botao)`
-  background-color: ${variaveis.verde};
-`
+
 export const BotaoCancelarRemover = styled(Botao)`
   background-color: ${variaveis.vermelho};
 `
