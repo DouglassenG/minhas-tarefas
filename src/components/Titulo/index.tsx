690a5081d0ec8e4954e07b62acfd05de88d55a15
Douglas <douglasmsrodrigues@hotmail.com>
2025-08-29 12:10:30 -0300

    Mundando a cor de fundo da barra lateral

--- src/components/Titulo/index.tsx
+++ src/components/Titulo/index.tsx
@@ -1,5 +1,5 @@
 import { Titulo } from './styles'
 
-const TituloPrincipal = () => <Titulo>Lista tarefas</Titulo>
+const TituloPrincipal = () => <Titulo>Lista de tarefas</Titulo>
 
 export default TituloPrincipal
--- src/containers/BarraLateral/styles.ts
+++ src/containers/BarraLateral/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const Aside = styled.aside`
   padding: 16px;
-  background-color: #eee;
+  background: linear-gradient(to right, #e9ffe9ff, #c9f3f7ff);
   height: 100vh;
 
   @media (max-width: 768px) {
@@ -11,8 +11,15 @@ export const Aside = styled.aside`
 `
 
 export const Filtros = styled.div`
+  background-size: cover;
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-gap: 8px;
   margin-top: 16px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 14px;
+    margin-top: 16px;
+  }
 `
